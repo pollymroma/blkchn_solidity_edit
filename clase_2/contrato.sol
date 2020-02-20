28ee8aa0c1580d8503ecbec63ccee70ce7d6a094
Paola <romanellopm@gmail.com>
2020-02-19 21:50:26 -0300

    agrego variable

--- clase_2/contrato.sol
+++ clase_2/contrato.sol
@@ -2,9 +2,11 @@ pragma solidity ^0.4.17;
 
 contract EjemploPrimerContrato {
     string public message;
+    string private finalMessage;
     
-    function EjemploPrimerContrato(string initialMessage) public {
-        message = initialMessage; 
+    function EjemploPrimerContrato(string initialMessage, string otherMessage) public {
+        message = initialMessage;
+        finalMessage = otherMessage;
     }
     
     function myMessage() public view returns (string) {
@@ -13,5 +15,9 @@ contract EjemploPrimerContrato {
     
     function getExpression() public pure returns (string) {
         return "Bievenido a Solidity";
+    }
+    
+    function myFinalMessage() public view returns (string) {
+        return finalMessage;
     }
 }
